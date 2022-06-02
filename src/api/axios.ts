05621c50f44d4198ae71e8073e26aefa2a1f0bdb
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2022-06-03 00:00:46 +0600

    add auth header

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:3032/",
   timeout: 10000,
+  headers: {
+    Authorization: "Basic YWRtaW46cGNaNzY5WD0tLUxTSlgmK1c3",
+  },
 });
